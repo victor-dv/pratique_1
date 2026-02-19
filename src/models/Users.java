bf34bbee8dd90f4a26660fb1ae6f250b123a6bf1
victor-dv <js813417@gmail.com>
2026-02-18 21:46:32 -0300

    fit - atualizei a classe do user com uma lista para armazenar os eventos confirmado, e criei os metodos para confirmar e cancelar presença nos evento

--- src/models/Users.java
+++ src/models/Users.java
@@ -1,15 +1,23 @@
 package models;
 
+import java.util.List;
 
 public class Users {
     private String email;
     private String nome;
     private String cityResidence;
+    private List<Events> eventsConfirmed;
 
     public Users(String email, String nome, String cityResidence){
         this.email = email;
         this.nome = nome;
         this.cityResidence = cityResidence;
+    }
+    public boolean confirmEvent(Events events){
+        return this.eventsConfirmed.add(events);
+    }
+    public boolean cancelEvent(Events events){
+        return this.eventsConfirmed.remove(events);
     }
 
 
@@ -35,5 +43,13 @@ public class Users {
 
     public void setCityResidence(String cityResidence) {
         this.cityResidence = cityResidence;
+    }
+
+    public List<Events> getEventsConfirmed() {
+        return eventsConfirmed;
+    }
+
+    public void setEventsConfirmed(List<Events> eventsConfirmed) {
+        this.eventsConfirmed = eventsConfirmed;
     }
 }
